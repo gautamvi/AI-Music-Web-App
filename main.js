628df0fe2a5351ad217465867e899256a2d54f8f
gautamvi <85584126+gautamvi@users.noreply.github.com>
2022-03-24 19:45:38 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-n
+harry_potter_song = "";
+peter_pan_song = "";
+
+function preload(){
+    harry_potter_song = loadSound("harry_potter.mp3");
+    peter_pan_song = loadSound("peter_pan.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
