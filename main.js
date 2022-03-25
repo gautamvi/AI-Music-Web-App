d26550e70b851e2b9d8b070512f46f09b7c611d6
gautamvi <85584126+gautamvi@users.noreply.github.com>
2022-03-24 19:49:44 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(600,500);
+    canvas = createCanvas(700,600);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
 }
 
 function draw(){
-    image(video,0,0,600,500);
+    image(video,0,0,700,600);
 }
