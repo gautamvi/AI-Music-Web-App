dd8a295b900ae4c8d6728a1d4f1e199a419d5293
gautamvi <85584126+gautamvi@users.noreply.github.com>
2022-03-29 16:45:45 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 harry_potter_song = "";
 peter_pan_song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
     harry_potter_song = loadSound("harry_potter.mp3");
@@ -11,8 +15,27 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Posenet is initialized");
 }
 
 function draw(){
     image(video,0,0,700,600);
+}
+
+function gotPoses(results){
+    if(results.length > 0 ){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+leftWristX+" leftWristY = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX+" rightWristY = "+rightWristY);
+    }
 }
